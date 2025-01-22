6133b30165a848d1f97fef0ce0b8e0d922d1d9ad
Celia506 <celia.sansano.dwwm48@gmail.com>
2025-01-22 11:49:07 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         primary: '#F2D8B3',
         second:'#D9B788',
         trois:'#FFFEFC',
+        quatre:'#FF9C1A',
+        cinq:'#E80505',      
       }
     },fontFamily: {
       relaway: ['Raleway','serif'],
