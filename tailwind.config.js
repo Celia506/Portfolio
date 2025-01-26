1298ddedf3c73f2b79398c4eeec0eff6440ef07b
Celia506 <celia.sansano.dwwm48@gmail.com>
2025-01-26 16:59:18 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,26 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors:{
-        zero:'#FDF5ED',
-        primary: '#F5E0C6',        
-        second:'#D9B788',
-        trois:'#FFFEFC',
-        quatre:'#FF9C1A',
-        cinq:'#E80505',
+      colors: {
+        zero: '#FDF5ED',
+        primary: '#F5E0C6',
+        second: '#D9B788',
+        trois: '#FFFEFC',
+        "50": "#fbf7f4", 
+        "100": "#efdfd3", 
+        "200": "#e2c7b2", 
+        "300": "#d5af91", 
+        "400": "#c79871", 
+        "500": "#b98151", 
+        "600": "#9f6b3f", 
+        "700": "#805633", 
+        "800": "#604126", 
+        "900": "#412c1a", 
+        "950": "#22170d" 
+        }
       }
-    },fontFamily: {
-      relaway: ['Raleway','serif'],
+    }, fontFamily: {
+  relaway: ['Raleway', 'serif'],
     },
-  },
-  plugins: [],
-}
+plugins: [],
+} 
